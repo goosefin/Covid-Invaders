bc54268ee5ed6da991722459427e8cb36fce5b42
goosefin <agustinafin96@gmail.com>
2022-07-12 21:40:43 -0400

    created instructions page

--- js/app.js
+++ js/app.js
@@ -20,13 +20,34 @@ const playerInputPage = () => {
     parentDiv.appendChild(player2)
     parentDiv.appendChild(player2Input)
     let gameButton = document.createElement('button')
-    gameButton.innerText = 'ENTER'
+    gameButton.innerText = 'NEXT'
     gameButton.setAttribute('id','game-button')
     parentDiv.append(gameButton)
-    const player1Name = document.getElementById('player-one-name').value
-    const player2Name = document.getElementById('player-two-name').value
+    // const player1Name = document.getElementById('player-one-name').value
+    // const player2Name = document.getElementById('player-two-name').value
 }
 
+const instructionsPage = () => {
+    let parentDiv = document.querySelector('#player-info')
+    parentDiv.remove()
+    let rulesHeading = document.createElement('p')
+    rulesHeading.setAttribute('id','rules-heading')
+    document.getElementById('instructions').appendChild(rulesHeading)
+    rulesHeading.innerText = 'How To Play'
+    let rules = document.createElement('p')
+    document.getElementById('instructions').appendChild(rules)
+    rules.innerText = 'Rounds are 90 seconds \n Player 1 moves with left and right arrows and shoots with right shift key \n Player 2 moves with A and D keys and shoots with left shift key \n Red covid cells are worth 1 point, blue cells are 5 points and green cells are 10 points \n Player with the highest score wins!'
+    let button = document.createElement('button')
+    button.setAttribute('id','enter-button')
+    document.getElementById('instructions').appendChild(button)
+    button.innerText = 'ENTER'
+}
 
+//event listeners
 document.querySelector('#start-button').addEventListener('click',landingPageRemoval)
 document.querySelector('#start-button').addEventListener('click',playerInputPage)
+document.querySelector('#player-info').addEventListener('click',function(e){
+    if(e.target && e.target.id == 'game-button'){
+        instructionsPage()
+    }
+})
