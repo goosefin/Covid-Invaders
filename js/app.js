87a05aae49eb7694f85e0963dd5c386ea2d90c2e
goosefin <agustinafin96@gmail.com>
2022-07-14 10:44:22 -0400

    player icons can move

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
 const player1 = document.getElementById('player-1-icon');
 const player2 = document.getElementById('player-2-icon');
 const canvas = document.getElementById('canvas');
+const canvas2 = document.getElementById('canvas2')
 const ctx = canvas.getContext('2d');
+const ctx2 = canvas2.getContext('2d');
 
-const player = {
-    w:50,
-    h:70,
-    x:20,
-    y:200,
+const firstPlayer = {
+    w:100,
+    h:100,
+    x:0,
+    y:550,
     speed: 5,
     dx: 0,
     dy: 0,
 }
 
 function drawPlayer() {
-    ctx.drawImage(player1, player.x, player.y, player.w, player.h)
+    ctx.drawImage(player1, firstPlayer.x, firstPlayer.y, firstPlayer.w, firstPlayer.h)
 }
 
 function clear(){
@@ -22,19 +24,19 @@ function clear(){
 }
 
 function newPos(){
-    player.x += player.dx;
-    player.y += player.dy;
+    firstPlayer.x += firstPlayer.dx;
+    firstPlayer.y += firstPlayer.dy;
     detectWalls()
 }
 
 function detectWalls(){
     //Left wall
-    if(player.x < 0){
-        player.x = 0
+    if(firstPlayer.x < 0){
+        firstPlayer.x = 0
     }
     //Right wall
-    if(player.x + player.w > canvas.width){
-        player.x = canvas.width -player.w
+    if(firstPlayer.x + firstPlayer.w > canvas.width){
+        firstPlayer.x = canvas.width - firstPlayer.w
     }
 }
 
@@ -45,20 +47,12 @@ function update() {
     requestAnimationFrame(update)
 }
 
-function moveUp(){
-    player.dy = -player.speed
-}
-
-function moveDown(){
-    player.dy = player.speed
-}
-
 function moveRight(){
-    player.dx = player.speed
+    firstPlayer.dx = firstPlayer.speed
 }
 
 function moveLeft(){
-    player.dx = -player.speed
+    firstPlayer.dx = -firstPlayer.speed
 }
 
 function keyDown(e){
@@ -66,21 +60,91 @@ function keyDown(e){
         moveRight()
     }else if(e.key === 'ArrowLeft'){
         moveLeft()
-    }else if(e.key === 'ArrowUp'){
-        moveUp()
-    }else if(e.key === 'ArrowDown'){
-        moveDown()
     }
 }
 
 function keyUp(e){
-    if(e.key === 'ArrowRight' || e.key === 'ArrowLeft' || e.key === 'ArrowUp' || e.key === 'ArrowDown'){
-        player.dx = 0;
-        player.dy = 0;
+    if(e.key === 'ArrowRight' || e.key === 'ArrowLeft'){
+        firstPlayer.dx = 0;
+        firstPlayer.dy = 0;
     }
 }
 
 update()
-
 document.addEventListener('keydown', keyDown)
 document.addEventListener('keyup', keyUp)
+
+// --------------------- SECOND PLAYER ---------------------//
+const secondPlayer = {
+    w:100,
+    h:100,
+    x:1000,
+    y:550,
+    speed: 5,
+    dx: 0,
+    dy: 0,
+}
+
+function drawPlayer2() {
+    ctx2.drawImage(player2, secondPlayer.x, secondPlayer.y, secondPlayer.w, secondPlayer.h)
+}
+
+function clear2(){
+    ctx2.clearRect(0, 0, canvas2.width, canvas2.height)
+}
+
+function newPos2(){
+    secondPlayer.x += secondPlayer.dx;
+    secondPlayer.y += secondPlayer.dy;
+    detectWalls2()
+}
+
+function detectWalls2(){
+    //Left wall
+    if(secondPlayer.x < 0){
+        secondPlayer.x = 0
+    }
+    //Right wall
+    if(secondPlayer.x + secondPlayer.w > canvas2.width){
+        secondPlayer.x = canvas2.width -secondPlayer.w
+    }
+}
+
+function update2() {
+    clear2()
+    drawPlayer2()
+    newPos2()
+    requestAnimationFrame(update2)
+}
+
+function moveRight2(){
+    secondPlayer.dx = secondPlayer.speed
+}
+
+function moveLeft2(){
+    secondPlayer.dx = -secondPlayer.speed
+}
+
+function keyDown2(e){
+    if(e.code === 'KeyD'){
+        moveRight2()
+    }else if(e.code === 'KeyA'){
+        moveLeft2()
+    }
+}
+
+function keyUp2(e){
+    if(e.code === 'KeyD' || e.code === 'KeyA'){
+        secondPlayer.dx = 0;
+        secondPlayer.dy = 0;
+    }
+}
+
+function test(e){
+    console.log(e.key)
+}
+
+update2()
+document.addEventListener('keydown', keyDown2)
+document.addEventListener('keyup', keyUp2)
+
