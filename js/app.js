faaefa051f6c89469a05aabed5d385369cb89f70
goosefin <agustinafin96@gmail.com>
2022-07-16 16:09:15 -0400

    send help

--- js/app.js
+++ js/app.js
@@ -1,74 +1,93 @@
 const player1 = document.getElementById('player-1-icon');
 const player2 = document.getElementById('player-2-icon');
 const canvas = document.getElementById('canvas');
-const canvas2 = document.getElementById('canvas2')
-const canvas3 = document.getElementById('canvas3')
 const ctx = canvas.getContext('2d');
-const ctx2 = canvas2.getContext('2d');
-const ctx3 = canvas3.getContext('2d')
-const player1Drop = document.querySelector('#blue-drop')
-const player2Drop = document.querySelector('#red-drop')
-const dropArray = []
+const dropArray = [];
+// let shoot = false;
+let timer = document.querySelector('#timer');
 
-const firstPlayer = {
-    w:100,
-    h:100,
-    x:0,
-    y:550,
-    speed: 5,
-    dx: 0,
-    dy: 0,
+//Sets up timer
+let counter = 90
+const runTimer = () =>{
+    if(counter > 0){
+        counter -= 1
+        timer.innerText = `Time: ${counter}`
+    }
 }
 
-const secondPlayer = {
-    w:100,
-    h:100,
-    x:1000,
-    y:550,
-    speed: 5,
-    dx: 0,
-    dy: 0,
+//setInterval(runTimer,1000)
+
+//creates the game object
+const game = {
+    firstPlayer: {
+        w:100,
+        h:100,
+        x:0,
+        y:550,
+        speed: 5,
+        dx: 0,
+        dy: 0,
+        score: 0
+    },
+    secondPlayer: {
+        w:100,
+        h:100,
+        x:1000,
+        y:550,
+        speed: 5,
+        dx: 0,
+        dy: 0,
+        score:0
+    }  
 }
 
 function drawPlayer() {
-    ctx.drawImage(player1, firstPlayer.x, firstPlayer.y, firstPlayer.w, firstPlayer.h)
-    ctx.drawImage(player2, secondPlayer.x, secondPlayer.y, secondPlayer.w, secondPlayer.h)
+    ctx.drawImage(player1, game.firstPlayer.x, game.firstPlayer.y, game.firstPlayer.w, game.firstPlayer.h)
+    ctx.drawImage(player2, game.secondPlayer.x, game.secondPlayer.y, game.secondPlayer.w, game.secondPlayer.h)
 }
 
 function clear(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    console.log('in clear')
 }
 
 function newPos(){
-    firstPlayer.x += firstPlayer.dx;
-    firstPlayer.y += firstPlayer.dy;
-    secondPlayer.x += secondPlayer.dx;
-    secondPlayer.y += secondPlayer.dy;
+    game.firstPlayer.x += game.firstPlayer.dx;
+    game.firstPlayer.y += game.firstPlayer.dy;
+    game.secondPlayer.x += game.secondPlayer.dx;
+    game.secondPlayer.y += game.secondPlayer.dy;
     detectWalls()
 }
 
-function detectWalls(player){
+function detectWalls(){
     //Left wall
-    if(firstPlayer.x < 0){
-        firstPlayer.x = 0
+    if(game.firstPlayer.x < 0){
+        game.firstPlayer.x = 0
     }
-    if(secondPlayer.x < 0){
-        secondPlayer.x = 0
+    if(game.secondPlayer.x < 0){
+        game.secondPlayer.x = 0
     }
     //Right wall
-    if(firstPlayer.x + firstPlayer.w > canvas.width){
-        firstPlayer.x = canvas.width - firstPlayer.w
+    if(game.firstPlayer.x + game.firstPlayer.w > canvas.width){
+        game.firstPlayer.x = canvas.width - game.firstPlayer.w
         
     }
-    if(secondPlayer.x + secondPlayer.w > canvas2.width){
-        secondPlayer.x = canvas2.width -secondPlayer.w
+    if(game.secondPlayer.x + game.secondPlayer.w > canvas.width){
+        game.secondPlayer.x = canvas.width - game.secondPlayer.w
+    }
+}
+const animate = () =>{
+    for(let bullet of dropArray){
+        bullet.update()
+        // console.log('in animate')
     }
 }
 
 function update() {
-    clear()
+    //clear()
     drawPlayer()
     newPos()
+    animate()
     requestAnimationFrame(update)
 }
 
@@ -82,23 +101,23 @@ function moveLeft(player){
 
 function keyDown(e){
     if(e.code === 'KeyD'){
-        moveRight(firstPlayer)
+        moveRight(game.firstPlayer)
     }else if(e.code === 'KeyA'){
-        moveLeft(firstPlayer)
+        moveLeft(game.firstPlayer)
     }else if(e.code === 'ArrowRight'){
-        moveRight(secondPlayer)
+        moveRight(game.secondPlayer)
     }else if(e.code === 'ArrowLeft'){
-        moveLeft(secondPlayer)
+        moveLeft(game.secondPlayer)
     }
 }
 
 function keyUp(e){
     if(e.code === 'KeyD' || e.code === 'KeyA'){
-        firstPlayer.dx = 0;
-        firstPlayer.dy = 0;
+        game.firstPlayer.dx = 0;
+        game.firstPlayer.dy = 0;
     }else if(e.code === 'ArrowLeft' || e.code === 'ArrowRight'){
-        secondPlayer.dx = 0
-        secondPlayer.dy = 0
+        game.secondPlayer.dx = 0
+        game.secondPlayer.dy = 0
     }
 }
 
@@ -106,157 +125,77 @@ update()
 document.addEventListener('keydown', keyDown)
 document.addEventListener('keyup', keyUp)
 
-// --------------------- SECOND PLAYER ---------------------//
-
-
-// function drawPlayer2() {
-//     ctx2.drawImage(player2, secondPlayer.x, secondPlayer.y, secondPlayer.w, secondPlayer.h)
-// }
-
-// function clear2(){
-//     ctx2.clearRect(0, 0, canvas2.width, canvas2.height)
-// }
-
-// function newPos2(){
-    
-// }
-
-// function detectWalls2(){
-//     //Left wall
-//     if(secondPlayer.x < 0){
-//         secondPlayer.x = 0
-//     }
-//     //Right wall
-//     if(secondPlayer.x + secondPlayer.w > canvas2.width){
-//         secondPlayer.x = canvas2.width -secondPlayer.w
-//     }
-// }
-
-// function update2() {
-//     clear2()
-//     drawPlayer2()
-//     newPos2()
-//     requestAnimationFrame(update2)
-// }
-
-// function moveRight2(){
-//     secondPlayer.dx = secondPlayer.speed
-// }
-
-// function moveLeft2(){
-//     secondPlayer.dx = -secondPlayer.speed
-// }
-
-// function keyDown2(e){
-//     if(e.key === 'ArrowRight'){
-//         moveRight2()
-//     }else if(e.key === 'ArrowLeft'){
-//         moveLeft2()
-//     }
-// }
-
-// function keyUp2(e){
-//     if(e.key === 'ArrowRight' || e.key === 'ArrowLeft'){
-//         secondPlayer.dx = 0;
-//         secondPlayer.dy = 0;
-//     }
-// }
-
-class Drop {
-    constructor(image, x){
-        this.image = new Image()
-        this.image.src = image
-        this.x = x
-        this.y = 550
-        this.w = 50
-        this.h = 50
-        this.dy = 0
-        this.dx = 0
-        this.speed = 5
+class Bullet {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 20
     }
-    drawDrop(){
-        ctx3.drawImage(player1Drop, this.x, this.y, this.w, this.h)
+    drawBullet(){
+        ctx.beginPath()
+        ctx.arc(this.position.x, this.position.y, this.raduis, 0, Math.PI*2)
+        ctx.fillStyle = 'black'
+        ctx.fill()
+        ctx.closePath()
+        //console.log(this.position.y)
     }
-    clearDrop(){
-        ctx3.clearRect(0, 0, canvas.width, canvas.height)
-    }
-    dropUpdate(){
-        this.clearDrop()
-        this.drawDrop()
-        requestAnimationFrame(this.dropUpdate)
-    }
-    moveDrop(){
-        this.y -= this.speed
-    }
-    shoot(player){
-        this.clearDrop()
-        this.drawDrop()
-        requestAnimationFrame(this.dropUpdate)
-        if(player.x === 1000){
-            this.x = player.x - 15
-        }else{
-            this.x = player.x
-        }
-        setInterval(this.moveDrop,10)
+    update(){
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        this.drawBullet()
+        //console.log('in update')
     }
 }
+
+// const dropUpdate = (drop) => {
+//     drop.clearDrop()
+//     drop.drawDrop()
+//     requestAnimationFrame(dropUpdate)
+// }
+
+
 
 const shotCheck = (e) => {
     if(e.code === 'ShiftLeft'){
-        const drop = new Drop ('https:://i.imgur.com/KqKypNk.png',firstPlayer.x)
-        dropArray.push(drop)
-        drop.shoot(firstPlayer)
+        dropArray.push(new Bullet({
+            position:{
+                x: 300,
+                y: 300
+            },
+            velocity:{
+                x:0,
+                y: -.02
+            }
+        }))
+        console.log('shot')
+        console.log(dropArray)
     }
 }
 
-// const firstDrop = {
-//     x: 30,
-//     y: 550,
-//     w: 40,
-//     h: 40,
-//     dx: 0,
-//     dy: 0,
-//     speed: 5,
-// }
-
-// function drawDrop(){
-//     ctx3.drawImage(player1Drop, firstDrop.x, firstDrop.y, firstDrop.w, firstDrop.h)
-// }
-
-// function clearDrop(){
-//     ctx3.clearRect(0, 0, canvas.width, canvas.height)
-// }
-
-// const dropTest = () => {
-//     clearDrop()
-//     drawDrop()
-//     requestAnimationFrame(dropTest)
-// }
-
-// const moveDrop = () => {
-//     firstDrop.y -= firstDrop.speed
-// }
-
-// const moveWithPlayer = () =>{
-//     firstDrop.x = firstPlayer.x + 30
-// }
-// setInterval(moveWithPlayer,1);
-
-// const shotCheck = (e) => {
-//     setInterval(()=>{
-//         if(e.code === 'ShiftRight'){
-//             moveDrop()
-//         }
-//     })
-// }
+const newBullet = new Bullet({
+    position:{
+        x: 300,
+        y: 300
+    },
+    velocity:{
+        x:0,
+        y: -5
+    }
+})
+dropArray.push(newBullet)
 
 
-//setInterval(moveDrop,10)
-//dropTest()
 
-update2()
-document.addEventListener('keydown', keyDown2)
-document.addEventListener('keyup', keyUp2)
+const checkWinner = () => {
+    if(game.firstPlayer.score > game.secondPlayer.score){
+        const winnerPopUp = document.createElement('h2')
+        winnerPopUp.innerText='Player 1 Wins!!'
+    }else if(game.secondPlayer.score > game.firstPlayer.score){
+        const winnerPopUp2 = document.createElement('h2')
+        winnerPopUp2.innerText='Player 2 Wins!!'
+    }
+}
 
+
+//dropUpdate()
 document.addEventListener('keydown',shotCheck)
-
