3402f612f9092f8a15a635dc813b3ed940c9721f
goosefin <agustinafin96@gmail.com>
2022-07-16 18:02:58 -0400

    bullets are kind of working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const player1 = document.getElementById('player-1-icon');
 const player2 = document.getElementById('player-2-icon');
 const canvas = document.getElementById('canvas');
+const canvas2 = document.getElementById('canvas2');
 const ctx = canvas.getContext('2d');
-const dropArray = [];
+const ctx2 = canvas2.getContext('2d');
+const shotsArray = [];
 // let shoot = false;
 let timer = document.querySelector('#timer');
 
@@ -38,6 +40,15 @@ const game = {
         dx: 0,
         dy: 0,
         score:0
+    },
+    checkWinner(){
+        if(this.firstPlayer.score > this.secondPlayer.score){
+            const winnerPopUp = document.createElement('h2')
+            winnerPopUp.innerText='Player 1 Wins!!'
+        }else if(this.secondPlayer.score > this.firstPlayer.score){
+            const winnerPopUp2 = document.createElement('h2')
+            winnerPopUp2.innerText='Player 2 Wins!!'
+        }
     }  
 }
 
@@ -48,7 +59,7 @@ function drawPlayer() {
 
 function clear(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    console.log('in clear')
+    //console.log('in clear')
 }
 
 function newPos(){
@@ -77,14 +88,14 @@ function detectWalls(){
     }
 }
 const animate = () =>{
-    for(let bullet of dropArray){
+    for(let bullet of shotsArray){
         bullet.update()
         // console.log('in animate')
     }
 }
 
 function update() {
-    //clear()
+    clear()
     drawPlayer()
     newPos()
     animate()
@@ -125,77 +136,90 @@ update()
 document.addEventListener('keydown', keyDown)
 document.addEventListener('keyup', keyUp)
 
+// class Bullet {
+//     constructor({position, velocity}){
+//         this.position = position
+//         this.velocity = velocity
+//         this.radius = 5
+//     }
+//     clear(){
+//         ctx.clearRect(0, 0, canvas.width, canvas.height)
+//         //console.log('in clear')
+//     }
+//     drawBullet(){
+//         this.clear()
+//         ctx.beginPath()
+//         ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2)
+//         ctx.fillStyle = 'black'
+//         ctx.fill()
+//         ctx.closePath()
+//         //console.log(`pos(${this.position.x},${this.position.y}, rad: ${this.radius} vel(${this.velocity.x}, ${this.velocity.y})`);
+//         }
+//     update2(){
+//         this.clear()
+//         this.drawBullet()
+//         this.position.x += this.velocity.x
+//         this.position.y += this.velocity.y
+//         //console.log('in update')
+//     }
+    
+// }
+
 class Bullet {
     constructor({position, velocity}){
         this.position = position
         this.velocity = velocity
-        this.radius = 20
+        this.radius = 5
+    }
+    clear(){
+        ctx2.clearRect(0, 0, canvas.width, canvas.height)
     }
     drawBullet(){
-        ctx.beginPath()
-        ctx.arc(this.position.x, this.position.y, this.raduis, 0, Math.PI*2)
-        ctx.fillStyle = 'black'
-        ctx.fill()
-        ctx.closePath()
-        //console.log(this.position.y)
+        this.clear()
+        ctx2.fillStyle = 'black'
+        ctx2.beginPath()
+        ctx2.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2)
+        ctx2.fill()
+        ctx2.closePath()
+        //console.log(`pos(${this.position.x},${this.position.y}, rad: ${this.radius} vel(${this.velocity.x}, ${this.velocity.y})`);
     }
     update(){
+        this.drawBullet()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
-        this.drawBullet()
         //console.log('in update')
     }
 }
 
-// const dropUpdate = (drop) => {
-//     drop.clearDrop()
-//     drop.drawDrop()
-//     requestAnimationFrame(dropUpdate)
-// }
-
-
 
 const shotCheck = (e) => {
     if(e.code === 'ShiftLeft'){
-        dropArray.push(new Bullet({
+        shotsArray.push(new Bullet({
             position:{
-                x: 300,
-                y: 300
+                x: game.firstPlayer.x + 30,
+                y: game.firstPlayer.y
             },
             velocity:{
                 x:0,
-                y: -.02
+                y: -5
             }
         }))
         console.log('shot')
-        console.log(dropArray)
-    }
+        console.log(shotsArray)
+    } 
 }
 
-const newBullet = new Bullet({
-    position:{
-        x: 300,
-        y: 300
-    },
-    velocity:{
-        x:0,
-        y: -5
-    }
-})
-dropArray.push(newBullet)
-
-
-
-const checkWinner = () => {
-    if(game.firstPlayer.score > game.secondPlayer.score){
-        const winnerPopUp = document.createElement('h2')
-        winnerPopUp.innerText='Player 1 Wins!!'
-    }else if(game.secondPlayer.score > game.firstPlayer.score){
-        const winnerPopUp2 = document.createElement('h2')
-        winnerPopUp2.innerText='Player 2 Wins!!'
-    }
-}
-
+// const newBullet = new Bullet({
+//     position:{
+//         x: 300,
+//         y: 300
+//     },
+//     velocity:{
+//         x:0,
+//         y: -5
+//     }
+// })
+//dropArray.push(newBullet)
 
 //dropUpdate()
 document.addEventListener('keydown',shotCheck)
