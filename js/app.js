d611cccfc0ae3670c6258da0e0c2a128c7e6ed46
goosefin <agustinafin96@gmail.com>
2022-07-19 21:20:37 -0400

    resturcured game object

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//VARIABLES
 const player1 = document.getElementById('player-1-icon');
 const player2 = document.getElementById('player-2-icon');
 const canvas = document.getElementById('canvas');
@@ -11,42 +12,43 @@ let playerTwoCount = document.querySelector('#player-two')
 let playerOneShot = false
 let playerTwoShot = false
 
+//INTRO TO THE GAME, INSTRUCTIONS AND NAME INPUTS
+alert("WELCOME TO COVID INVADERS")
+const playerOneName = prompt('Player 1: enter your name.')
+const playerTwoName = prompt('Player 2: enter your name.')
+alert(`${playerOneName}, you will play as BLUE. Move with KEYS A and D. Shoot the cells with SHIFT LEFT.`)
+alert(`${playerTwoName}, you will play as RED. Move with ARROW LEFT and RIGHT. Shoot the cells with SHIFT RIGHT.`)
+alert('Rounds will last 60 seconds! Player with the most cells shot down wins! Press OK to start game.')
 
-// alert("WELCOME TO COVID INVADERS")
-// const playerOneName = prompt('Player 1: enter your name.')
-// const playerTwoName = prompt('Player 2: enter your name.')
-// // alert(`${playerOneName}, you will play as blue. Move left and right with key's 'A' and 'D'. Shoot the cells with shift left.`)
-// // alert(`${playerTwoName}, you will play as blue. Move with arrow left and right. Shoot the cells with shift right.`)
-// // alert('Rounds will last 60 seconds! Player with the most cells shot down wins! Press OK to start game.')
-
-// // playerOneCount.innerText = `${playerOneName}:`
-// // playerTwoCount.innerText = `${playerTwoName}:`
+playerOneCount.innerText = `${playerOneName}:`
+playerTwoCount.innerText = `${playerTwoName}:`
 
 
-// //Sets up timer
-// let counter = 10
-// const runTimer = () =>{
-//     if(counter > 0){
-//         counter -= 1
-//         timer.innerText = `Time: ${counter}`
-//     }
-// }
+//TIMER FOR COUNTDOWN CLOCK
+let counter = 10
+const runTimer = () =>{
+    if(counter > 0){
+        counter -= 1
+        timer.innerText = `Time: ${counter}`
+    }
+}
 
-// let timerCount = setInterval(() => {
-//     runTimer()
-//     if(counter == 0){
-//         game.checkWinner()
-//         clearInterval(timerCount)
-//         clearInterval(cellSpawn)
-//         timer.innerText = 'Game Over'
-//         setTimeout(() => {
-//             alert('Play again?')
-//             location.reload()
-//         },5000)
-//     }
-// },1000)
+//COUNTDOWN CLOCK END
+let timerCount = setInterval(() => {
+    runTimer()
+    if(counter == 0){
+        game.checkWinner()
+        clearInterval(timerCount)
+        clearInterval(cellSpawn)
+        timer.innerText = 'Game Over'
+        setTimeout(() => {
+            alert('Play again?')
+            location.reload()
+        },5000)
+    }
+},1000)
 
-//creates the game object
+//GAME OBJECT WITH PLAYERS AND METHODS
 const game = {
     firstPlayer: {
         w:100,
@@ -82,193 +84,42 @@ const game = {
             document.body.appendChild(tiePopUp)
             tiePopUp.innerText = 'Players Tied!!'
         }
-    }
-}
-
-function drawPlayer() {
-    ctx.drawImage(player1, game.firstPlayer.x, game.firstPlayer.y, game.firstPlayer.w, game.firstPlayer.h)
-    ctx.drawImage(player2, game.secondPlayer.x, game.secondPlayer.y, game.secondPlayer.w, game.secondPlayer.h)
-}
-
-function clear(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-}
-
-function newPos(){
-    game.firstPlayer.x += game.firstPlayer.dx;
-    game.firstPlayer.y += game.firstPlayer.dy;
-    game.secondPlayer.x += game.secondPlayer.dx;
-    game.secondPlayer.y += game.secondPlayer.dy;
-    detectWalls()
-}
-
-function detectWalls(){
-    //Left wall
-    if(game.firstPlayer.x < 0){
-        game.firstPlayer.x = 0
-    }
-    if(game.secondPlayer.x < 0){
-        game.secondPlayer.x = 0
-    }
-    //Right wall
-    if(game.firstPlayer.x + game.firstPlayer.w > canvas.width){
-        game.firstPlayer.x = canvas.width - game.firstPlayer.w
-        
-    }
-    if(game.secondPlayer.x + game.secondPlayer.w > canvas.width){
-        game.secondPlayer.x = canvas.width - game.secondPlayer.w
-    }
-}
-
-const animate = () =>{
-    shotsArray.forEach(bullet => {
-        bullet.update()
-    })
-    cellsArray.forEach((cell, index) =>{
-        if(cell.position.y >= 660){
-            cellsArray.splice(index,1)
-            console.log('inside if statement')
-        }else{
-            cell.updateCell()
+    },
+    drawPlayer(){
+        ctx.drawImage(player1, this.firstPlayer.x, this.firstPlayer.y, this.firstPlayer.w, this.firstPlayer.h)
+        ctx.drawImage(player2, this.secondPlayer.x, this.secondPlayer.y, this.secondPlayer.w, this.secondPlayer.h)
+    },
+    clear(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+    },
+    newPos(){
+        this.firstPlayer.x += this.firstPlayer.dx;
+        this.firstPlayer.y += this.firstPlayer.dy;
+        this.secondPlayer.x += this.secondPlayer.dx;
+        this.secondPlayer.y += this.secondPlayer.dy;
+        this.detectWalls()
+    },
+    detectWalls(){
+        //LEFT WALL DETECTION
+        if(this.firstPlayer.x < 0){
+            this.firstPlayer.x = 0
         }
-    })
-    shotsArray.forEach((bullet, i) =>{
-        cellsArray.forEach((cell,j) => {
-            if(bullet.position.x <= cell.position.x + 15 && bullet.position.x >= cell.position.x - 15 && bullet.position.y <= cell.position.y + 15 && bullet.position.y >= cell.position.y - 15)  {
-                // cellsArray.splice(j, 1)
-                // shotsArray.splice(i, 1)
-                if(playerOneShot){
-                    cellsArray.splice(j, 1)
-                    shotsArray.splice(i, 1)
-                    game.firstPlayer.score += 1
-                    playerOneCount.innerText = `${playerOneName}: ${game.firstPlayer.score}`
-                    console.log('inside player 1 score')
-                }else if(playerTwoShot){
-                    cellsArray.splice(j, 1)
-                    shotsArray.splice(i, 1)
-                    game.secondPlayer.score += 1
-                    playerTwoCount.innerText = `${playerTwoName}: ${game.secondPlayer.score}`
-                    console.log('inside player 2 score')
-                }
-            }
-        })
-    })
-}
-
-
-function update() {
-    clear()
-    drawPlayer()
-    newPos()
-    animate()
-    requestAnimationFrame(update)
-}
-
-function moveRight(player){
-    player.dx = player.speed
-}
-
-function moveLeft(player){
-    player.dx = -player.speed
-}
-
-function keyDown(e){
-    if(e.code === 'KeyD'){
-        moveRight(game.firstPlayer)
-    }else if(e.code === 'KeyA'){
-        moveLeft(game.firstPlayer)
-    }else if(e.code === 'ArrowRight'){
-        moveRight(game.secondPlayer)
-    }else if(e.code === 'ArrowLeft'){
-        moveLeft(game.secondPlayer)
+        if(this.secondPlayer.x < 0){
+            this.secondPlayer.x = 0
+        }
+        //RIGHT WALL DETECTION
+        if(this.firstPlayer.x + this.firstPlayer.w > canvas.width){
+            this.firstPlayer.x = canvas.width - this.firstPlayer.w
+            
+        }
+        if(this.secondPlayer.x + this.secondPlayer.w > canvas.width){
+            this.secondPlayer.x = canvas.width - this.secondPlayer.w
+        }
     }
+
 }
 
-function keyUp(e){
-    if(e.code === 'KeyD' || e.code === 'KeyA'){
-        game.firstPlayer.dx = 0;
-        game.firstPlayer.dy = 0;
-    }else if(e.code === 'ArrowLeft' || e.code === 'ArrowRight'){
-        game.secondPlayer.dx = 0
-        game.secondPlayer.dy = 0
-    }
-}
-
-update()
-document.addEventListener('keydown', keyDown)
-document.addEventListener('keyup', keyUp)
-
-class Bullet {
-    constructor({position, velocity}){
-        this.position = position
-        this.velocity = velocity
-        this.radius = 5
-    }
-    drawBullet(){
-        ctx.fillStyle = 'black'
-        ctx.beginPath()
-        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2)
-        ctx.fill()
-        ctx.closePath()
-    }
-    update(){
-        this.drawBullet()
-        this.position.y += this.velocity.y
-    }
-}
-
-
-const player1Shoot = (e) => {
-    if(e.code === 'ShiftLeft'){
-        playerOneShot = true
-        shotsArray.push(new Bullet({
-            position:{
-                x: game.firstPlayer.x + game.firstPlayer.w / 2,
-                y: game.firstPlayer.y
-            },
-            velocity:{
-                x:0,
-                y: -5
-            }
-        }))
-    }
-}
-
-const player2Shoot = (e) => {
-    if(e.code === 'ShiftRight'){
-        playerTwoShot = true
-        shotsArray.push(new Bullet({
-            position:{
-                x: game.secondPlayer.x + game.secondPlayer.w / 2,
-                y: game.secondPlayer.y
-            },
-            velocity:{
-                x:0,
-                y:-5
-            }
-        }))
-    }
-}
-
-const player1KeyDownShoot = (e) => {
-    if(e.code == 'ShiftLeft'){
-        playerOneShot = false;
-    }
-}
-
-const player2KeyDownShoot = (e) => {
-    if(e.code == 'ShiftRight'){
-        playerTwoShot = false;
-    }
-}
-
-document.addEventListener('keydown',player1Shoot)
-document.addEventListener('keydown',player2Shoot)
-document.addEventListener('keyup',player1KeyDownShoot)
-document.addEventListener('keyup',player2KeyDownShoot)
-
-//covid cells 
-
+//CELL CLASS CONSTRUCTOR
 class Cell {
     constructor({position,velocity}){
         this.position = position
@@ -288,17 +139,7 @@ class Cell {
     }
 }
 
-// cellsArray.push(new Cell({
-//         position:{
-//             x:Math.floor(Math.random() * canvas.width),
-//             y:300
-//         },
-//         velocity:{
-//             x:0,
-//             y:0
-//         }
-// }))
-
+//RANDOMLY GENERATED CELL SPAWNS
 const cellSpawn = setInterval(()=>{
     cellsArray.push(new Cell({
         position:{
@@ -310,16 +151,157 @@ const cellSpawn = setInterval(()=>{
             y:3
         }
     }))
-    //console.log('in interval')
-    //console.log(cellsArray)
 },1000)
 
-// shotsArray.forEach(bullet =>{
-//     cellsArray.forEach(cell =>{
-//         if(bullet.position.y == cell.position.y + 8 || bullet.position.y == cell.position.y - 8 
-//         && bullet.position.x == cell.position.x + 8 || bullet.position.y == cell.position.x - 8){
-//             cellsArray.splice(cellsArray[cell],1)
-//             // shotsArray.splice(index,1)
-//         }
-//     })
-// })
+//BULLET CLASS CONSTRUCTOR
+class Bullet {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 5
+    }
+    drawBullet(){
+        ctx.fillStyle = 'black'
+        ctx.beginPath()
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2)
+        ctx.fill()
+        ctx.closePath()
+    }
+    update(){
+        this.drawBullet()
+        this.position.y += this.velocity.y
+    }
+}
+
+//ANIMATION LOOPS FOR BULLET AND CELLS
+//COLLISION DETECTION
+const animate = () =>{
+    shotsArray.forEach(bullet => {
+        bullet.update()
+    })
+    cellsArray.forEach((cell, index) =>{
+        if(cell.position.y >= 660){
+            cellsArray.splice(index,1)
+            console.log('inside if statement')
+        }else{
+            cell.updateCell()
+        }
+    })
+    shotsArray.forEach((bullet, i) =>{
+        cellsArray.forEach((cell,j) => {
+            if(bullet.position.x <= cell.position.x + 15 && bullet.position.x >= cell.position.x - 15 && bullet.position.y <= cell.position.y + 15 && bullet.position.y >= cell.position.y - 15)  {
+                if(playerOneShot){
+                    cellsArray.splice(j, 1)
+                    shotsArray.splice(i, 1)
+                    game.firstPlayer.score += 1
+                    playerOneCount.innerText = `${playerOneName}: ${game.firstPlayer.score}`
+                }else if(playerTwoShot){
+                    cellsArray.splice(j, 1)
+                    shotsArray.splice(i, 1)
+                    game.secondPlayer.score += 1
+                    playerTwoCount.innerText = `${playerTwoName}: ${game.secondPlayer.score}`
+                }
+            }
+        })
+    })
+}
+
+//UPDATE LOOP TO ANIMATE PLAYER AND CALL BACK TO ANIMATE BULLETS AND CELLS
+const update = () => {
+    game.clear()
+    game.drawPlayer()
+    game.newPos()
+    animate()
+    requestAnimationFrame(update)
+}
+
+//MOVE RIGHT
+const moveRight = (player) => {
+    player.dx = player.speed
+}
+
+//MOVE LEFT
+const moveLeft = (player) => {
+    player.dx = -player.speed
+}
+
+//FUNCTION FOR PLAYER MOVEMENTS
+const keyDown = (e) => {
+    if(e.code === 'KeyD'){
+        moveRight(game.firstPlayer)
+    }else if(e.code === 'KeyA'){
+        moveLeft(game.firstPlayer)
+    }else if(e.code === 'ArrowRight'){
+        moveRight(game.secondPlayer)
+    }else if(e.code === 'ArrowLeft'){
+        moveLeft(game.secondPlayer)
+    }
+}
+
+//CLEARING FOR PLAYER MOVEMENT 
+const keyUp = (e) => {
+    if(e.code === 'KeyD' || e.code === 'KeyA'){
+        game.firstPlayer.dx = 0;
+        game.firstPlayer.dy = 0;
+    }else if(e.code === 'ArrowLeft' || e.code === 'ArrowRight'){
+        game.secondPlayer.dx = 0
+        game.secondPlayer.dy = 0
+    }
+}
+
+//PLAYER ONE SHOOTING FUNCTION
+const player1Shoot = (e) => {
+    if(e.code === 'ShiftLeft'){
+        playerOneShot = true
+        shotsArray.push(new Bullet({
+            position:{
+                x: game.firstPlayer.x + game.firstPlayer.w / 2,
+                y: game.firstPlayer.y
+            },
+            velocity:{
+                x:0,
+                y: -5
+            }
+        }))
+    }
+}
+
+//PLAYER TWO SHOOTING FUNCTION
+const player2Shoot = (e) => {
+    if(e.code === 'ShiftRight'){
+        playerTwoShot = true
+        shotsArray.push(new Bullet({
+            position:{
+                x: game.secondPlayer.x + game.secondPlayer.w / 2,
+                y: game.secondPlayer.y
+            },
+            velocity:{
+                x:0,
+                y:-5
+            }
+        }))
+    }
+}
+
+//PLAYER ONE SHOOTING VARIABLE UPDATE
+const player1KeyDownShoot = (e) => {
+    if(e.code == 'ShiftLeft'){
+        playerOneShot = false;
+    }
+}
+
+//PLAYER TWO SHOOTING VARIABLE UPDATE
+const player2KeyDownShoot = (e) => {
+    if(e.code == 'ShiftRight'){
+        playerTwoShot = false;
+    }
+}
+
+//EVENT HANDLER LISTENING
+update()
+document.addEventListener('keydown', keyDown)
+document.addEventListener('keyup', keyUp)
+document.addEventListener('keydown',player1Shoot)
+document.addEventListener('keydown',player2Shoot)
+document.addEventListener('keyup',player1KeyDownShoot)
+document.addEventListener('keyup',player2KeyDownShoot)
